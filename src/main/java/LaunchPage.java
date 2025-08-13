66334195d7c47589bff22a794c67e82439ee5ab2
reg-ooo <regorego2006@gmail.com>
2025-08-13 22:37:20 +0800

    day1

---
+++ src/main/java/LaunchPage.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+
+public class LaunchPage extends JFrame{
+    JFrame mainFrame = new JFrame();
+    public LaunchPage(){
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(1920, 1080);
+        mainFrame.setVisible(true);
+    }
+}
--- src/main/java/Payday.java
+++ src/main/java/Payday.java
@@ -1,5 +1,31 @@
-public class Payday {
-    public static void main(String[] args) {
+import java.sql.*;
 
+public class Payday {
+    static String db,uname,pswd;
+    static Connection con;
+    static Statement st;
+    public static void main(String[] args) {
+//        new LaunchPage();
+        DBConnect();
+    }
+    
+    public static void DBConnect(){
+        db = "banksystem";
+        uname = "root";
+        pswd = "Rego061800@#";
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://127.0.0.1:3307/?serverTimezone=UTC";
+            con = DriverManager.getConnection(url, uname, pswd);
+            st = con.createStatement();
+
+            System.out.println("Connected to MySQL database successfully!");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to database: " + e.getMessage());
+        }
     }
 }
