d2c5a9a5508443e73080a375a01b7baf570bafaf
reg-ooo <regorego2006@gmail.com>
2025-08-26 08:08:54 +0800

    Add files via upload

---
+++ src/main/java/Style.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.io.InputStream;
+
+public final class Style {
+
+    //HIMO KO ANING CLASS PARA DI NA BALIK-BALIK BUHAT OG COLOR KADA PAGE
+    public static final Color gray = new Color(0xD3D3D3);
+    public static final Color pBlue = new Color(0x2D76BD);
+    public static final Color dBlue = new Color(0x163F5C);
+    public static final Color lBlue = new Color(0xC4E4FF);
+    public static final Color black = new Color(0x000000);
+    public static final Color white = new Color(0xFFFFFF);
+    public static final Color transparent = new Color(0,0,0,0);
+
+
+    public Font loadFont(int style, float size, String fontName) {
+        try {
+            InputStream fontStream = getClass().getResourceAsStream("/fonts/" + fontName + ".ttf");
+            Font karlaFont = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+            return karlaFont.deriveFont(style, size);
+        } catch (Exception e) {
+            System.out.println("Could not load font: " + e.getMessage());
+
+            return new Font("Arial", style, (int)size);
+        }
+    }
+}
