438a876753f0ffe26b9d1f1acf3e3342a5423fb4
comic11 <pulidoomicoo@gmail.com>
2025-09-02 20:54:51 +0800

    a
    a

--- src/main/java/pages/LaunchPage.java
+++ src/main/java/pages/LaunchPage.java
@@ -2,35 +2,22 @@ package pages;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.geom.*;
-import java.io.*;
 import java.util.ArrayList;
 import components.*;
 import panels.*;
 
-public class LaunchPage extends JFrame {
+public class LaunchPage extends JPanel {
     private Style style = new Style();
-    RoundedFrame mainFrame = new RoundedFrame(30);
-    final ImageIcon appLogo = new ImageIcon("appLogo.png");
-
-    NPanel nPanel = new NPanel();
-    TransactionPanel tPanel = new TransactionPanel();
-    NavigationBar navBarPanel = new NavigationBar();
-    CenterPanel centerPanel = new CenterPanel();
+    private NPanel nPanel = new NPanel();
+    private TransactionPanel tPanel = new TransactionPanel();
+    private NavigationBar navBarPanel = new NavigationBar();
+    private CenterPanel centerPanel = new CenterPanel();
 
     ArrayList<RoundedBorder> buttons = new ArrayList<>();
-    public LaunchPage(){
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setSize(420, 650);
-        mainFrame.setLocationRelativeTo(null);
-        mainFrame.setTitle("Payday Bank");
-        mainFrame.setIconImage(appLogo.getImage());
-        mainFrame.setResizable(false);
-        mainFrame.setLayout(new BorderLayout());
-        mainFrame.setUndecorated(true);
-        mainFrame.setBackground(Color.WHITE);
-        mainFrame.getContentPane().setBackground(Color.WHITE);
+
+    public LaunchPage() {
+        setLayout(new BorderLayout());
+        setBackground(Color.WHITE);
 
         JPanel mainContentPanel = new JPanel();
         CardLayout cardLayout = new CardLayout();
@@ -38,12 +25,10 @@ public class LaunchPage extends JFrame {
         mainContentPanel.add(centerPanel, "main");
         mainContentPanel.add(tPanel, "transaction");
 
-        mainFrame.add(nPanel, BorderLayout.NORTH);
-        mainFrame.add(mainContentPanel, BorderLayout.CENTER);
-        mainFrame.add(navBarPanel, BorderLayout.SOUTH);
+        add(nPanel, BorderLayout.NORTH);
+        add(mainContentPanel, BorderLayout.CENTER);
+        add(navBarPanel, BorderLayout.SOUTH);
 
         cardLayout.show(mainContentPanel, "main");
-
-        mainFrame.setVisible(true);
     }
 }
